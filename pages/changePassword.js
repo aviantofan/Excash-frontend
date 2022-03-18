f9075394f438029cbf27d17c6566ab1b5a10389e
eexxprd <aviantofan@gmail.com>
2022-03-19 06:38:44 +0700

    feat: add init change password

---
+++ pages/changePassword.js
@@ -0,0 +1,56 @@
+import SideBar from '../components/SideBar'
+import Image from 'next/image'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Input from '../components/Input'
+import Button from '../components/Button'
+
+const changePassword = () => {
+  return (
+    <>
+      <Header />
+      <div className='container mt-4 mb-4'>
+        <div className='row'>
+          <div className='col-3 sideBar bg-color5' key='sideBar'>
+            <SideBar />
+          </div>
+          <div className='col'>
+            <div className='container dataProfile bg-color5'>
+              <div className='row'>
+                <div className='col-6 mt-2 fw-bold'>Change Password</div>
+              </div>
+              <div className='row mt-3 align-items-center'>
+                <div className='col-lg'>
+                  <div>You must enter your current password and then<br /> type your new password twice.</div>
+                </div>
+              </div>
+              <div className='row mt-5' >
+                <div className='col-12 text-center'>
+                  <Input type='password' className='input-underline text-start bg-color5 text-color4 w-50' placeholder='Current password' />
+                </div>
+              </div>
+              <div className='row mt-5' >
+                <div className='col-12 text-center'>
+                  <Input type='password' className='input-underline text-start bg-color5 text-color4 w-50' placeholder='New password' />
+                </div>
+              </div>
+              <div className='row mt-5' >
+                <div className='col-12 text-center'>
+                  <Input type='password' className='input-underline text-start bg-color5 text-color4 w-50' placeholder='Repeat new password' />
+                </div>
+              </div>
+              <div className='row mt-5' >
+                <div className='col-12 text-center'>
+                  <Button type='submit' className='change w-50' >Change Password</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default changePassword
