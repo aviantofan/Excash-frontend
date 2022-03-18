1d795371cb7cdaac5abab2695d6c6f6b325cab63
eexxprd <aviantofan@gmail.com>
2022-03-18 08:18:16 +0700

    feat: add layout history

--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,13 @@
 import Chart from '../components/Chart'
 import SideBar from '../components/SideBar'
 import Button from '../components/Button'
+import Image from 'next/image'
 
 const home = () => {
+  const history = [
+    { image: '/images/2.png', name: 'Samuel Suhi', status: 'Accept', value: '50000' },
+    { image: '/images/1.png', name: 'Christine', status: 'Pending', value: '150000' }
+  ]
   return (
     <div className='container'>
       <div className='row'>
@@ -28,11 +33,32 @@ const home = () => {
                 </div>
               </div>
             </div>
-            <div className='col-7'>
+            <div className='col-7 mt-2'>
               <Chart data={[10, 50, 20, 0, 10, 40, 100]} labels={['sat', 'sun', 'mon', 'tue', 'wed', 'thu', 'fri']} income={100000} expense={10000} />
             </div>
-            <div className='col-5'>
-              x
+            <div className='col-5 mt-2'>
+              <div className='row'>
+                <div className='col-6'>Transaction History</div>
+                <div className='col text-end'>See all</div>
+              </div>
+              <div className='row mt-2'>
+                {history.map(item => {
+                  return (
+                    <>
+                      <div className='col-3 mb-2' key='history'>
+                        <Image src={item.image} width={56} height={56} alt='user' />
+                      </div>
+                      <div className='col-5'>
+                        <div>{item.name}</div>
+                        <div>{item.status}</div>
+                      </div>
+                      <div className='col text-end'>
+                        <div>{item.value}</div>
+                      </div>
+                    </>
+                  )
+                })}
+              </div>
             </div>
           </div>
         </div>
