cb50cbd0faa5109bc7f3ffccb9854e26ab0cd312
eexxprd <aviantofan@gmail.com>
2022-03-19 00:56:47 +0700

    feat: add init

---
+++ pages/transferInput.js
@@ -0,0 +1,64 @@
+import SideBar from '../components/SideBar'
+import Image from 'next/image'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Input from '../components/Input'
+import Button from '../components/Button'
+
+const transferInput = () => {
+  return (
+    <>
+      <Header />
+      <div className='container mt-4 mb-4'>
+        <div className='row'>
+          <div className='col-3 sideBar bg-color5' key='sideBar'>
+            <SideBar />
+          </div>
+          <div className='col'>
+            <div className='container dataTransfer bg-color5'>
+              <div className='row'>
+                <div className='col-6 mb-3 text'>Transfer Money</div>
+              </div>
+              <div className='row mt-2 data bg-white align-items-center'>
+                <div className='col-lg-1 mb-3 pt-4' key='history'>
+                  <Image src='/images/2.png' width={56} height={56} alt='user' />
+                </div>
+                <div className='col-lg-11'>
+                  <div>Samuel Suhi</div>
+                  <div>+62 813-8492-9994</div>
+                </div>
+              </div>
+              <div className='row mt-4'>
+                <div>Type the amount you want to transfer and then <br />
+                  press continue to the next steps.</div>
+              </div>
+              <div className='row mt-4'>
+                <div className='col-12'>
+                  <Input type='text' className='input-amount text-center text-color4 w-100' placeholder='0.00' />
+                </div>
+              </div>
+              <div className='row mt-3'>
+                <div className='col-12 text-center'>
+                  <div>Rp{Number('120000').toLocaleString('id-ID')} Available</div>
+                </div>
+              </div>
+              <div className='row mt-3' >
+                <div className='col-12 text-center'>
+                  <Input type='text' className='input-underline text-start bg-color5 text-color4 w-50' placeholder='Add some notes' />
+                </div>
+              </div>
+              <div className='row mt-3' >
+                <div className='col-12 text-end'>
+                  <Button type='text' className='transferInput'>Continue</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default transferInput
