ee9e2663efeabe3a86366215df59f891c74d4b68
eexxprd <aviantofan@gmail.com>
2022-03-19 08:13:09 +0700

    feat: add updates pin input

--- components/PinInput.js
+++ components/PinInput.js
@@ -1,6 +1,19 @@
-import PINInput from 'pin-input'
+import React, { Component } from 'react';
+import OtpInput from 'react-otp-input';
 
-<PINInput
-  type='password'
-  fields={6}
-/>
+export default class App extends Component {
+  state = { otp: '' };
+
+  handleChange = (otp) => this.setState({ otp });
+
+  render() {
+    return (
+      <OtpInput
+        value={this.state.otp}
+        onChange={this.handleChange}
+        numInputs={6}
+        separator={<span> </span>}
+      />
+    );
+  }
+}
